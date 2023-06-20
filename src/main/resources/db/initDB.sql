2884e7d0751583e922d6783a08c8522febcb8141
Филинов Анатолий Григорьевич <AGFilinov@sberbank.ru>
2023-06-20 18:18:22 +0300

    HW3 (with optional)

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,11 @@
-DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
+DROP SEQUENCE IF EXISTS global_seq_meal;
 
 CREATE SEQUENCE global_seq START WITH 100000;
+CREATE SEQUENCE global_seq_meal START WITH 100000;
 
 CREATE TABLE users
 (
@@ -16,10 +19,23 @@ CREATE TABLE users
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
-CREATE TABLE user_role
+CREATE TABLE user_roles
 (
     user_id INTEGER NOT NULL,
     role    VARCHAR NOT NULL,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq_meal'),
+    user_id     INTEGER CONSTRAINT user_id_fk REFERENCES users (id) ON DELETE CASCADE,
+    datetime    TIMESTAMP           DEFAULT now() NOT NULL,
+    description VARCHAR                           NOT NULL,
+    calories    INTEGER                           NOT NULL
+);
+CREATE INDEX user_id_idx ON meals (user_id);
+CREATE UNIQUE INDEX meal_id_user_id_idx ON meals (id, user_id);
+CREATE UNIQUE INDEX meals_unique_datetime_idx ON meals (dateTime, user_id);
+
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,12 +1,27 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
+DELETE
+FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE global_seq_meal RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
        ('Admin', 'admin@gmail.com', 'admin'),
        ('Guest', 'guest@gmail.com', 'guest');
 
-INSERT INTO user_role (role, user_id)
+INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, '2022-01-30 10:00:00', 'Завтрак', 500),
+       (100000, '2022-01-30 13:00:00', 'Обед', 1000),
+       (100000, '2022-01-30 20:00:00', 'Ужин', 500),
+       (100001, '2022-01-31 00:00:00', 'Еда на граничное значение', 100),
+       (100001, '2022-01-31 10:00:00', 'Завтрак', 1000),
+       (100001, '2022-01-31 13:00:00', 'Обед', 500),
+       (100001, '2022-01-31 20:00:00', 'Ужин', 410);
+
